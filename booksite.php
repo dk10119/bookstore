4e3dac24324959ca68fa426b6563572fc4c0de96
dk10119 <tranduclong092@gmail.com>
2024-03-05 12:42:52 +0200

    added display books from JSON function

--- booksite.php
+++ booksite.php
@@ -16,46 +16,49 @@
             <ul>
                 <li><a href="booksite.php">Home</a></li>
                 <li><a href="booksite.php?genre=adventure">Adventure</a></li>
-                <li><a href="booksite.php?genre=classic">Classic Literature</a></li>
+                <li><a href="booksite.php?genre=classic%20literature">Classic Literature</a></li>
                 <li><a href="booksite.php?genre=coming-of-age">Coming-of-age</a></li>
                 <li><a href="booksite.php?genre=fantasy">Fantasy</a></li>
-                <li><a href="booksite.php?genre=historical">Historical Fiction</a></li>
+                <li><a href="booksite.php?genre=historical%20fiction">Historical Fiction</a></li>
                 <li><a href="booksite.php?genre=horror">Horror</a></li>
                 <li><a href="booksite.php?genre=mystery">Mystery</a></li>
                 <li><a href="booksite.php?genre=romance">Romance</a></li>
-                <li><a href="booksite.php?genre=scifi">Science Fiction</a></li>
+                <li><a href="booksite.php?genre=science%20fiction">Science Fiction</a></li>
             </ul>
         </nav>
         <main>
-            <?php
-                // Here you should display the books of the given genre (GET parameter "genre"). Check the links above for parameter values.
-                // If the parameter is not set, display all books.
-
-                // Use the HTML template below and a loop (+ conditional if the genre was given) to go through the books in file  
-                
+            <?php           
                 // You also need to check the cookies to figure out if the book is favorite or not and display correct symbol.
                 // If the book is in the favorite list, add the class "fa-star" to the a tag with "bookmark" class.
                 // If not, add the class "fa-star-o". These are Font Awesome classes that add a filled star and a star outline respectively.
                 // Also, make sure to set the id parameter for each book, so the setfavorite.php page gets the information which book to favorite/unfavorite.
 
                 // Read the file into array variable $books:
-                $json = file_get_contents("books.json");
+                $json = file_get_contents("assets/books.json");
                 $books = json_decode($json, true);
+                
+                if (isset($_GET["genre"])) {
+                    $genre = $_GET["genre"];
+                    $filteredBooks = array_filter($books, fn($book) => $book["genre"] == ucwords($genre));
+                } else {
+                    $genre = "All";
+                    $filteredBooks = $books;
+                }
 
-            ?>
-            <h2>Genre Name or "All Books"</h2>
+                echo "<h2>" . ucwords($genre) . "</h2>";
 
-            <section class="book">
-                <a class="bookmark fa fa-star-o" href="setfavorite.php?id=1"></a>
-                <h3>To Kill a Mockingbird</h3>
-                <p class="publishing-info">
-                    <span class="author">Harper Lee</span>,
-                    <span class="year">1960</span>
-                </p>
-                <p class="description">
-                    Harper Lee's masterpiece explores racial injustice and moral growth through the eyes of a young girl in the American South.
-                </p>
-            </section>
+                foreach($filteredBooks as $book) { ?>
+                <section class="book">
+                    <a class="bookmark fa fa-star-o" href="setfavorite.php?id=<?php echo $book["id"] ?>"></a>
+                    <h3><?php echo $book["title"] ?></h3>
+                    <p class="publishing-info">
+                        <span class="author"><?php echo $book["author"] ?></span>,
+                        <span class="year"><?php echo $book["publishing_year"] ?></span>
+                    </p>
+                    <p class="description"><?php echo $book["description"] ?></p>
+                </section>
+
+            <?php } ?>
 
         </main>
     </div>    
